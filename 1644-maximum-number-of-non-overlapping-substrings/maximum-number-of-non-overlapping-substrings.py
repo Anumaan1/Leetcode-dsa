c370ffadbfed8546f029cbc3ffd2e9a50eb4c7d4
Anumaan Jaiswal <lakshyajaiswal2004@gmail.com>
2026-09-18 22:05:14 +0530

    Time: 173 ms (81.42%) | Memory: 20.3 MB (88.50%) - LeetSync

---
+++ 1644-maximum-number-of-non-overlapping-substrings/maximum-number-of-non-overlapping-substrings.py
@@ -0,0 +1,46 @@
+class Solution:
+    def maxNumOfSubstrings(self, s: str) -> list[str]:
+        first = {}
+        last = {}
+
+        # First and last occurrence
+        for i, ch in enumerate(s):
+            if ch not in first:
+                first[ch] = i
+            last[ch] = i
+
+        intervals = []
+
+        # Find valid intervals
+        for ch in first:
+            l = first[ch]
+            r = last[ch]
+            i = l
+            valid = True
+
+            while i <= r:
+                c = s[i]
+
+                # Character started before l -> invalid
+                if first[c] < l:
+                    valid = False
+                    break
+
+                r = max(r, last[c])
+                i += 1
+
+            if valid:
+                intervals.append((l, r))
+
+        # Select non-overlapping intervals
+        intervals.sort(key=lambda x: x[1])
+
+        ans = []
+        end = -1
+
+        for l, r in intervals:
+            if l > end:
+                ans.append(s[l:r + 1])
+                end = r
+
+        return ans
